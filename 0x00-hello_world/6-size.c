de44fdc13c419d6cdfe4889c92c3116a9777240d
markagugo <markagugo@gmail.com>
2022-06-02 14:29:35 -0700

    0000-HELLO_WORLD

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the size of various types
- *   * Return: 0 if exited properly, non-zero otherwise
- *    */
+ *   main - prints the size of various types
+ *   Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));

a6aec6f3fccc85f6f63f4031ee474b50dfc612f2
markagugo <markagugo@gmail.com>
2022-06-13 08:21:54 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverses a string
+ * @s: input to reverse
+ * Return: string and in reverse
+ */
+	void rev_string(char *s)
+	{
+		char rev = s[0];
+		int fcounter = 0;
+		int i;
+
+		while (s[fcounter] != '\0')
+			fcounter++;
+
+		for (i = 0; i < fcounter; i++)
+		{
+			fcounter--;
+			rev = s[i];
+			s[i] = s[fcounter];
+			s[fcounter] = rev;
+		}
+	}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: destination of the string
+ * @src: source of the string
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *guard = dest;
+
+while (*src)
+{
+*dest++ = *src++;
+*dest = 0;
+}
+return (guard);
+}

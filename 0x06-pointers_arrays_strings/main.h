f1d5a9e2ffb53946ad849765f4fcfed76b612f22
markagugo <markagugo@gmail.com>
2022-06-15 20:45:31 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,3 +8,5 @@ char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
 char *rot13(char *n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);

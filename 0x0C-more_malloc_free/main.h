b0002208bd9acce03020d114e881408c4c8695ac
markagugo <markagugo@gmail.com>
2022-06-30 07:21:50 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,5 +12,8 @@ int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
 void _puts(char *str);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif

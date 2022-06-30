5eda7d11bbe90dcc10093262f8c2c312b794f3ba
markagugo <markagugo@gmail.com>
2022-06-30 06:58:40 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,14 +1,16 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-int _putchar(char c);
+#include <stdlib.h>
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
+int _putchar(char c);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+void _puts(char *str);
 
 #endif

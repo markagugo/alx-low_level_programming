0d0b7133ea301a7bd5a769cb7b2505ddcf2ed7be
markagugo <markagugo@gmail.com>
2022-06-30 14:10:35 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,16 +1,11 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-#include <stdlib.h>
-
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _putchar(char c);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
-void _puts(char *str);
 
-#endif
+#endif /* FILE_MAIN */

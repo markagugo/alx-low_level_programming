2303ca4b720154997e406a89871cce5fb9c687e0
markagugo <markagugo@gmail.com>
2022-09-05 20:01:06 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc -g -fPIC -Wall -Werror -Wextra -pedantic *.c -shared -o liball.so
+gcc -c -fpic *.c
+gcc -shared *.o -o liball.so
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget https://github.com/svelezg/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
-export LD_PRELOAD=../inject.so

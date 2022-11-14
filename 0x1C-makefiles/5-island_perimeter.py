20d74ef4e67b7cd49cedfee7c2a1c15d00b2fa23
markagugo <markagugo@gmail.com>
2022-11-13 19:30:18 -0800

    0x1C-makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Solving a technical interview problem..."""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j-1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i-1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
